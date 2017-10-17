63864eca3e3e3ba3ba8c6eef66c0a140240fd6af
Sascha Spors <Sascha.Spors@uni-rostock.de>
2017-10-17 11:36:48 +0200

    Illustration of Fourier transforms and periodic sinc function

---
+++ images/mk_spectral_analysis.sh
@@ -0,0 +1,3 @@
+latex -shell-escape spectral_analysis.tex
+convert -units PixelsPerInch -density 144 -resize 600 spectral_analysis-1.png ../spectral_analysis_deterministic_signals/Fourier_transforms.png
+
